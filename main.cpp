fd04d158d2c2d3e4dae0e4530dc65916cfc52000
manuelzamoraperez <91272541+manuelzamoraperez@users.noreply.github.com>
2022-04-29 16:34:42 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,307 @@
+#include "RestClient.h"
+#include "ArduinoJson.h"
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+int test_delay = 1000; //so we don't spam the API
+boolean describe_tests = true;
+
+RestClient client = RestClient("192.168.56.1", 80);//ip modificar
+
+#define STASSID "PacoManu" //modificar no 5g
+#define STAPSK  "pacito123" // modificar
+
+//Setup
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(STASSID);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Setup!");
+
+  //setup sensor:
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+String response;
+
+String serializeBody(int termometroId, double temp, double hum)//poner nuestros params
+{
+  StaticJsonDocument<200> doc;
+
+  // StaticJsonObject allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument  doc(200);
+
+  // Add values in the document
+  //
+  //doc["sensor"] = termometro;
+  doc["idSensor"] =termometroId;
+  //doc["time"] = time;
+
+  // Add an array.
+  //
+  JsonArray data = doc.createNestedArray("data");
+  data.add(temp);
+  data.add(hum);
+
+  // Generate the minified JSON and send it to the Serial port.
+  //
+  String output;
+  serializeJson(doc, output);
+  // The above line prints:
+  // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+
+  // Start a new line
+  Serial.println(output);
+
+  // Generate the prettified JSON and send it to the Serial port.
+  //
+  //serializeJsonPretty(doc, output);
+  // The above line prints:
+  // {
+  //   "sensor": "gps",
+  //   "time": 1351824120,
+  //   "data": [
+  //     48.756080,
+  //     2.302038
+  //   ]
+  // }
+  return output;
+}
+
+void test_status(int statusCode)
+{
+  delay(test_delay);
+  if (statusCode == 200 || statusCode == 201)
+  {
+    Serial.print("TEST RESULT: ok (");
+    Serial.print(statusCode);
+    Serial.println(")");
+  }
+  else
+  {
+    Serial.print("TEST RESULT: fail (");
+    Serial.print(statusCode);
+    Serial.println(")");
+  }
+}
+
+void deserializeBody(String responseJson){
+  if (responseJson != "")
+  {
+    StaticJsonDocument<200> doc;
+
+    //char json[] =
+    //    "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, responseJson);
+
+    // Test if parsing succeeds.
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // Fetch values.
+    //
+    // Most of the time, you can rely on the implicit casts.
+    // In other case, you can do doc["time"].as<long>();
+    const char *sensor = doc["sensor"];
+    //long time = doc["time"];
+    double temp = doc["data"][0];
+    double hum = doc["data"][1];
+
+    // Print values.
+    Serial.println(sensor);
+    //Serial.println(time);
+    Serial.println(temp, 6);
+    Serial.println(hum, 6);
+  }
+}
+
+void test_response()
+{
+  Serial.println("TEST RESULT: (response body = " + response + ")");
+  response = "";
+}
+
+void describe(char *description)
+{
+  if (describe_tests)
+    Serial.println(description);
+}
+
+void GET_tests()
+{
+  describe("Test GET with path");
+  test_status(client.get("/api/sensors", &response));
+  test_response();
+
+  describe("Test GET with path and response");
+  test_status(client.get("/api/sensors", &response));
+  test_response();
+
+  describe("Test GET with path");
+  test_status(client.get("/api/sensors/123", &response));
+  test_response();
+}
+
+void POST_tests()
+{
+  String post_body = serializeBody(millis(), "dht", millis(), random(-20, 50)/10, random(0, 100)/10);
+  describe("Test POST with path and body and response");
+  test_status(client.post("/api/sensors", post_body.c_str(), &response));
+  test_response();
+}
+
+void PUT_tests()
+{
+  int temp = 38;
+  long timestamp = 151241254122;
+  // POST TESTS
+  String post_body = "{ 'idsensor' : 18, 'value': " + temp;
+  post_body = post_body + " , 'timestamp' :";
+  post_body = post_body + timestamp;
+  post_body = post_body + ", 'user' : 'Luismi'}";
+
+  describe("Test PUT with path and body");
+  test_status(client.put("/data/445654", post_body.c_str()));
+
+  describe("Test PUT with path and body and response");
+  test_status(client.put("/data/1241231", post_body.c_str(), &response));
+  test_response();
+
+  describe("Test PUT with path and body and header");
+  client.setHeader("X-Test-Header: true");
+  test_status(client.put("/data-header/1241231", post_body.c_str()));
+
+  describe("Test PUT with path and body and header and response");
+  client.setHeader("X-Test-Header: true");
+  test_status(client.put("/data-header/1241231", post_body.c_str(), &response));
+  test_response();
+
+  describe("Test PUT with 2 headers and response");
+  client.setHeader("X-Test-Header1: one");
+  client.setHeader("X-Test-Header2: two");
+  test_status(client.put("/data-headers/1241231", post_body.c_str(), &response));
+  test_response();
+}
+
+void DELETE_tests()
+{
+  int temp = 37;
+  long timestamp = 151241254122;
+  // POST TESTS
+  String post_body = "{ 'idsensor' : 18, 'value': " + temp;
+  post_body = post_body + " , 'timestamp' :";
+  post_body = post_body + timestamp;
+  post_body = post_body + ", 'user' : 'Luismi'}";
+
+  describe("Test DELETE with path");
+  //note: requires a special endpoint
+  test_status(client.del("/del/1241231"));
+
+  describe("Test DELETE with path and body");
+  test_status(client.del("/data/1241231", post_body.c_str()));
+
+  describe("Test DELETE with path and body and response");
+  test_status(client.del("/data", post_body.c_str(), &response));
+  test_response();
+
+  describe("Test DELETE with path and body and header");
+  client.setHeader("X-Test-Header: true");
+  test_status(client.del("/data-header", post_body.c_str()));
+
+  describe("Test DELETE with path and body and header and response");
+  client.setHeader("X-Test-Header: true");
+  test_status(client.del("/data-header", post_body.c_str(), &response));
+  test_response();
+
+  describe("Test DELETE with 2 headers and response");
+  client.setHeader("X-Test-Header1: one");
+  client.setHeader("X-Test-Header2: two");
+  test_status(client.del("/data-headers", post_body.c_str(), &response));
+  test_response();
+}
+
+// Run the tests!
+void loop()
+{
+  GET_tests();
+  POST_tests();
+  //PUT_tests();
+  //DELETE_tests();
+  // Delay between measurements.Sensor dht22
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+}
